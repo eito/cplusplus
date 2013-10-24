ec666704cae4c9bfd9cc008e79b430a9fee16f43
Eric Ito <eric.ito@gmail.com>
2013-10-24 13:45:06 -0700

    add NULL check

--- pointers/pointers/main.cpp
+++ pointers/pointers/main.cpp
@@ -10,6 +10,10 @@
 
 void print(int *begin, int *end)
 {
+    if (!begin || !end) {
+        std::cout << "One or more are NULL\n";
+        return;
+    }
     while (begin != end) {
         std::cout << *begin << " ";
         ++begin;
@@ -21,7 +25,11 @@ int main(int argc, const char * argv[])
 {
 
     int a[5];
-    a[0] = 1; a[1] = 2; a[2] = 3; a[3] = 4; a[4] = 5;
+    a[0] = 1;
+    a[1] = 2;
+    a[2] = 3;
+    a[3] = 4;
+    a[4] = 5;
     int *begin = a;
     print (begin, begin+5);
     return 0;
