f52be4693d5c00ef370407b2b0e472c6317044e8
Eric Ito <eric.ito@gmail.com>
2013-10-24 16:28:47 -0700

    start dynamic memory lab

---
+++ dynamic_memory_2/dynamic_memory_2/main.cpp
@@ -0,0 +1,75 @@
+//
+//  main.cpp
+//  dynamic_memory_2
+//
+//  Created by Eric Ito on 10/24/13.
+//  Copyright (c) 2013 Eric Ito. All rights reserved.
+//
+
+#include <iostream>
+
+using namespace std;
+
+class Number
+{
+public:
+    
+    Number() { m_size = 0; m_digits = 0; };
+    
+    ~Number()
+    {
+        delete [] m_digits;
+    };
+    
+    void read (istream& is)
+    {
+        delete [] m_digits;
+        
+        // read in string
+        string s;
+        is >> s;
+        //
+        // figure out size
+        m_size = (int)s.length();
+        m_digits = new ushort[m_size];
+        for (int i = 0; i < m_size; i++) {
+            m_digits[i] = s[i];
+        }
+        
+    };
+    void print(ostream& os) const
+    {
+        if (m_digits == 0)
+        {
+            os << "undefined";
+        }
+        else {
+            for (int i = 0; i < m_size; i++) {
+                os << m_digits[i] - '0';
+            }
+            os << endl;
+        }
+    }
+    bool defined() const
+    {
+        return (m_digits != 0);
+    }
+private:
+    int     m_size;
+    ushort* m_digits;
+};
+
+int main(int argc, const char * argv[])
+{
+
+    Number a;
+    if (!a.defined()) {
+        cout << "Not defined yet" << endl;
+    }
+    cout << "number? ";
+    a.read(cin);
+    a.print(cout);
+
+    return 0;
+}
+
