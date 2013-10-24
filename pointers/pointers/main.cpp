5b5a5c9becc173b4e57dfcaae638a4a9381dfa0d
Eric Ito <eric.ito@gmail.com>
2013-10-24 13:37:45 -0700

    update

--- pointers/pointers/main.cpp
+++ pointers/pointers/main.cpp
@@ -8,17 +8,22 @@
 
 #include <iostream>
 
+void print(int *begin, int *end)
+{
+    while (begin != end) {
+        std::cout << *begin << " ";
+        ++begin;
+    }
+    std::cout << std::endl;
+}
+
 int main(int argc, const char * argv[])
 {
 
     int a[5];
     a[0] = 1; a[1] = 2; a[2] = 3; a[3] = 4; a[4] = 5;
-
-    int *p = a;
-    for (int i  = 0; i < 5; ++i, ++p) {
-        std::cout << *p << " ";
-    }
-    std::cout << std::endl;
+    int *begin = a;
+    print (begin, begin+5);
     return 0;
 }
 
