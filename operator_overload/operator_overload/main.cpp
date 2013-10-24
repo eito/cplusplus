ab499adc7c99ef56d0979fd9c144cba1413d6d86
Eric Ito <eric.ito@gmail.com>
2013-10-24 14:19:42 -0700

    add overload operators

--- operator_overload/operator_overload/main.cpp
+++ operator_overload/operator_overload/main.cpp
@@ -8,11 +8,43 @@
 
 #include <iostream>
 
+class Point
+{
+public:
+    int x;
+    int y;
+
+    Point(int x, int y) {
+        this->x = x;
+        this->y = y;
+    };
+    
+    Point operator+(Point q) {
+        Point r = Point(0,0);
+        r.x = this->x + q.x;
+        r.y = this->y + q.y;
+        return r;
+    };
+    
+    Point& operator+=(Point p) {
+        x += p.x;
+        y += p.y;
+        return *this;
+    };
+    
+};
+
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    Point t = Point(1,1);
+    Point s = Point(2,2);
+    Point z = t + s;
+    std::cout << "x = " << z.x << " and y = " << z.y << std::endl;
+    
+    t += s;
+    std::cout << "x = " << t.x << " and y = " << t.y << std::endl;
+
     return 0;
 }
 
