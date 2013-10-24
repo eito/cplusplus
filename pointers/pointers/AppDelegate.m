196427f8eddef9d1ffab92755b94184ab56a35ce
Eric Ito <eric.ito@gmail.com>
2013-10-24 13:32:36 -0700

    add gitignore

---
+++ pointers/pointers/AppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  AppDelegate.m
+//  pointers
+//
+//  Created by Eric Ito on 10/24/13.
+//  Copyright (c) 2013 Eric Ito. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+    // Insert code here to initialize your application
+}
+
+@end
---
+++ pointers/pointers/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  pointers
+//
+//  Created by Eric Ito on 10/24/13.
+//  Copyright (c) 2013 Eric Ito. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[])
+{
+    return NSApplicationMain(argc, argv);
+}
