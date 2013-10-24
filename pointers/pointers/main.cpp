4aac694201844a576c9fb54ee265c7dbac640c5e
Eric Ito <eric.ito@gmail.com>
2013-10-24 13:33:09 -0700

    add pointers

--- pointers/pointers/main.cpp
+++ pointers/pointers/main.cpp
@@ -11,8 +11,14 @@
 int main(int argc, const char * argv[])
 {
 
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    int a[5];
+    a[0] = 1; a[1] = 2; a[2] = 3; a[3] = 4; a[4] = 5;
+
+    int *p = a;
+    for (int i  = 0; i < 5; ++i, ++p) {
+        std::cout << *p << " ";
+    }
+    std::cout << std::endl;
     return 0;
 }
 
