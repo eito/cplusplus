eb5400cecc3571f8913e3088e7558ee8647fbb86
Eric Ito <eric.ito@gmail.com>
2013-10-24 14:19:27 -0700

    update gitignore

---
+++ operator_overload/operator_overload/main.cpp
@@ -0,0 +1,18 @@
+//
+//  main.cpp
+//  operator_overload
+//
+//  Created by Eric Ito on 10/24/13.
+//  Copyright (c) 2013 Eric Ito. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
+
